9c321fef31ff6f276544b79817ee115702abc3e9
GhouI <abdulalian201@gmail.com>
2024-02-13 21:08:51 +0000

    first commit

---
+++ NotesApp/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿namespace NotesApp.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        
+        public string NoteName { get; set; }
+        public string NoteContent { get; set; }
+        public string NoteType { get; set; }
+        public Note() { }
+
+    }
+}
